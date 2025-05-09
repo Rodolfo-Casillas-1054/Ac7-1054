bcf827f8996092f40fedc5f074bee8d9f21936d2
RODOLFO_ANTONIO_CASILLAS_GARCIA <a.22308051281054@cbtis128.edu.mx>
2025-05-09 16:23:57 +0000

    HOlaa<aaaaaaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:act7_1054/inicio.dart';
+import 'package:act7_1054/p-uno.dart';
+import 'package:act7_1054/p-dos.dart';
+import 'package:act7_1054/p-tres.dart';
+import 'package:act7_1054/p-cuatro.dart';
+import 'package:act7_1054/p-cinco.dart';
+import 'package:act7_1054/p-seis.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MisRutas());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MisRutas extends StatelessWidget {
+  const MisRutas({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Rutas entre paginas',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const FSN(),
+        '/second': (context) => const PG1(),
+        '/third': (context) => const PG2(),
+        '/fourth': (context) => const PG3(),
+        '/fifth': (context) => const PG4(),
+        '/sixth': (context) => const PG5(),
+        '/seventh': (context) => const PG6(),
+      },
     );
   }
 }
